3a2b723230a317a354332cc6f27bdc7c9aee7b79
aidannairn <aidan.nairn95@gmail.com>
2022-07-14 16:00:11 +1200

    Modify user signup so that columns are appended to row dynamically, dependant on whether new user is a student, or teacher.

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -6,22 +6,52 @@ const { getENV } = require('../config/dotenv')
 const dbName = getENV('database')
 
 const signUpUser = (req, res) => {
-  const { firstName, lastName, email, password, school, profilePic, dateOfBirth, contactNumber } = req.body
+  const { type, teacherID, firstName, lastName, email, password, school, profilePic, dateOfBirth, contactNumber, course } = req.body
 
   if (!email) {
     return res.status(400).send('Missing parameter: email')
   }
+
   if (!password) {
     return res.status(400).send('Missing parameter: password')
   }
-
   
   try {
     const hashedPassword = bcrypt.hashSync(password, 10)
+    // By default - Set table to Student
+    let table = 'Student'
+
+    const colsWithValues = [
+      { TeacherID: teacherID },
+      { FirstName: firstName },
+      { LastName: lastName },
+      { Email: email },
+      { Password: hashedPassword },
+      { School: school },
+      { ProfilePic: profilePic },
+      { DateOfBirth: dateOfBirth },
+      { ContactNumber: contactNumber },
+      { Course: course }
+    ]
+    // If type included in request and equal to "Teacher" - Remove unnecessary columns
+    if (type === 'Teacher') {
+      table = 'Teacher'
+      colsWithValues.shift() // Remove first - "TeacherID"
+      colsWithValues.pop() // Remove last - "Course"
+    }
+
+    const keysArr = colsWithValues.map(col => Object.keys(col)[0])
+    const colsStr = keysArr.join(', ')
+    
+    // (?, ?, ? etc) - MySQL syntax for values to be 
+    let valuesPlaceholder = colsWithValues.map(() => '?').join(', ')
+
+    const valuesArr = colsWithValues.map(col => col[Object.keys(col)[0]])
+
     //  Call an INSERT query into the DB
     dbConnection.query(
-      `INSERT INTO ${dbName}.Teacher (FirstName, LastName, Email, Password, School, ProfilePic, DateOfBirth, ContactNumber) VALUES (?, ?, ?, ?, ?, ?, ?, ?)`,
-      [firstName, lastName, email, hashedPassword, school, profilePic, dateOfBirth, contactNumber]
+      `INSERT INTO ${dbName}.${table} (${colsStr}) VALUES (${valuesPlaceholder})`,
+      valuesArr
     )
     res.send(`Created a new User with the email: ${email}.`)
   } catch (error) {
