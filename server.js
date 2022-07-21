96598d04b984c144926e8f4fbca34844c6bb5858
aidannairn <aidan.nairn95@gmail.com>
2022-07-22 10:35:20 +1200

    A student can log in and store a refresh token in the database to stay logged in.

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const cookieParser = require('cookie-parser') 
 const cors = require('cors')
 
 const { getENV } = require('./src/config/dotenv')
@@ -7,8 +8,9 @@ const { projectBuilderRouter } = require('./src/routes/projectBuilder.routes.js'
 
 const app = express()
 
+app.use(cors({ credentials:true, origin: getENV('corsOrigin') }))
+app.use(cookieParser())
 app.use(express.json())
-app.use(cors())
 
 app.use(userRouter)
 app.use('/student/project', projectBuilderRouter)
